787d04d5f0b7e104e2e8e434f057337e14ab3738
namikkaya <namikkaya86@gmail.com>
2020-02-02 18:57:05 +0300

    Initial Commit

---
+++ onenewone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onenewone
+//
+//  Created by namik kaya on 2.02.2020.
+//  Copyright © 2020 namik kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
